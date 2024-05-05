9b8dc561687eea9527dc0efa7c8ec5796d97b128
sirkaue <kauekdslopes@gmail.com>
2024-05-04 23:35:35 -0300

    Trocando para série

--- src/main/java/com/sirkaue/screenmatch/model/DadosFilme.java
+++
@@ -1,8 +0,0 @@
-package com.sirkaue.screenmatch.model;
-
-import com.fasterxml.jackson.annotation.JsonAlias;
-
-public record DadosFilme(@JsonAlias("Title") String titulo,
-                         @JsonAlias("Runtime") String minutos,
-                         @JsonAlias("imdbRating") String avaliacao) {
-}
