5f6c562afec99b43618ee91dd596991ff2fe17c4
sirkaue <kauekdslopes@gmail.com>
2024-06-20 18:49:22 -0300

    Mapeando entidades

--- src/main/java/com/sirkaue/screenmatch/model/Serie.java
+++ src/main/java/com/sirkaue/screenmatch/model/Serie.java
@@ -1,14 +1,24 @@
 package com.sirkaue.screenmatch.model;
 
 import com.sirkaue.screenmatch.service.traducao.ConsultaMyMemory;
+import jakarta.persistence.*;
 
 import java.util.OptionalDouble;
 
+@Entity
+@Table(name = "series")
 public class Serie {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
     private String titulo;
     private Integer totalTemporadas;
     private Double avaliacao;
+
+    @Enumerated(EnumType.STRING)
     private Categoria genero;
     private String atores;
     private String poster;
@@ -22,6 +32,14 @@ public class Serie {
         this.atores = dadosSerie.atores();
         this.poster = dadosSerie.poster();
         this.sinopse = ConsultaMyMemory.obterTraducao(dadosSerie.sinopse()).trim();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitulo() {
