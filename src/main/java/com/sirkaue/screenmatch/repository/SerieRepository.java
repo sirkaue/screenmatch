bfae9d2158443caf6f76164cd886f06d28ad8913
sirkaue <kauekdslopes@gmail.com>
2024-06-20 19:29:21 -0300

    Criando Repository

---
+++ src/main/java/com/sirkaue/screenmatch/repository/SerieRepository.java
@@ -0,0 +1,7 @@
+package com.sirkaue.screenmatch.repository;
+
+import com.sirkaue.screenmatch.model.Serie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SerieRepository extends JpaRepository<Serie, Long> {
+}
