5b91417c9ee69e58435f19087a2ee0da5812d43b
sirkaue <kauekdslopes@gmail.com>
2024-06-20 17:06:37 -0300

    Implementado API de tradução

---
+++ src/main/java/com/sirkaue/screenmatch/service/traducao/DadosResposta.java
@@ -0,0 +1,8 @@
+package com.sirkaue.screenmatch.service.traducao;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosResposta( @JsonAlias(value = "translatedText") String textoTraduzido) {
+}
