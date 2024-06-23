7807f51e6244e0153b3d06340c97dce2522ee9e7
sirkaue <kauekdslopes@gmail.com>
2024-06-23 13:26:05 -0300

    Corrigindo imports

--- src/main/java/com/sirkaue/screenmatch/menu/Menu.java
+++ src/main/java/com/sirkaue/screenmatch/menu/Menu.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class Menu {
     Scanner sc = new Scanner(System.in);
