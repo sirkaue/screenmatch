fcd0c1228d15e21f491b2d27991c9e4b771b3765
sirkaue <kauekdslopes@gmail.com>
2024-06-27 13:14:13 -0300

    Conhecendo a JPQL - series por temporada e avaliação

--- src/main/java/com/sirkaue/screenmatch/repository/SerieRepository.java
+++ src/main/java/com/sirkaue/screenmatch/repository/SerieRepository.java
@@ -3,6 +3,7 @@ package com.sirkaue.screenmatch.repository;
 import com.sirkaue.screenmatch.model.Categoria;
 import com.sirkaue.screenmatch.model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,4 +18,7 @@ public interface SerieRepository extends JpaRepository<Serie, Long> {
     List<Serie> findByGenero(Categoria categoria);
 
     List<Serie> findByTotalTemporadasLessThanEqualAndAvaliacaoGreaterThanEqual(int totalTemporadas, double avaliacao);
+
+    @Query("SELECT s FROM Serie s WHERE s.totalTemporadas <= :totalTemporadas AND s.avaliacao >= :avaliacao")
+    List<Serie> seriesPorTemporadaEAvaliacao(int totalTemporadas, double avaliacao);
 }
