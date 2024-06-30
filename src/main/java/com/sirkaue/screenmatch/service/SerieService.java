79b0869242762cb75bb361e9f7dfa3f93dd47f0b
sirkaue <kauekdslopes@gmail.com>
2024-06-30 17:55:24 -0300

    Updating service

--- src/main/java/com/sirkaue/screenmatch/service/SerieService.java
+++ src/main/java/com/sirkaue/screenmatch/service/SerieService.java
@@ -1,11 +1,13 @@
 package com.sirkaue.screenmatch.service;
 
 import com.sirkaue.screenmatch.dto.SerieDto;
+import com.sirkaue.screenmatch.model.Serie;
 import com.sirkaue.screenmatch.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -15,8 +17,36 @@ public class SerieService {
     private SerieRepository repository;
 
     public List<SerieDto> findAll() {
-        return repository.findAll()
-                .stream().map(s -> new SerieDto(s.getId(),
+        return convertEntityToDtoList(repository.findAll());
+    }
+
+    public List<SerieDto> findByTop5Series() {
+        return convertEntityToDtoList(repository.findTop5ByOrderByAvaliacaoDesc());
+    }
+
+    public List<SerieDto> findLatestReleases() {
+        return convertEntityToDtoList(repository.latestReleases());
+    }
+
+    public SerieDto findById(Long id) {
+        Optional<Serie> serie = repository.findById(id);
+
+        if (serie.isPresent()) {
+            Serie s = serie.get();
+            return new SerieDto(s.getId(),
+                    s.getTitulo(),
+                    s.getTotalTemporadas(),
+                    s.getAvaliacao(),
+                    s.getGenero(),
+                    s.getAtores(),
+                    s.getPoster(),
+                    s.getSinopse());
+        }
+        return null;
+    }
+
+    private List<SerieDto> convertEntityToDtoList(List<Serie> series) {
+        return series.stream().map(s -> new SerieDto(s.getId(),
                         s.getTitulo(),
                         s.getTotalTemporadas(),
                         s.getAvaliacao(),
