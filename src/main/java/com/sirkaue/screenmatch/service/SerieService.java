b4693b247e0fb8cfb4214672e6da7c80fc76a52a
sirkaue <kauekdslopes@gmail.com>
2024-06-30 14:03:03 -0300

    service - findAll

---
+++ src/main/java/com/sirkaue/screenmatch/service/SerieService.java
@@ -0,0 +1,29 @@
+package com.sirkaue.screenmatch.service;
+
+import com.sirkaue.screenmatch.dto.SerieDto;
+import com.sirkaue.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SerieService {
+
+    @Autowired
+    private SerieRepository repository;
+
+    public List<SerieDto> findAll() {
+        return repository.findAll()
+                .stream().map(s -> new SerieDto(s.getId(),
+                        s.getTitulo(),
+                        s.getTotalTemporadas(),
+                        s.getAvaliacao(),
+                        s.getGenero(),
+                        s.getAtores(),
+                        s.getPoster(),
+                        s.getSinopse()))
+                .collect(Collectors.toList());
+    }
+}
