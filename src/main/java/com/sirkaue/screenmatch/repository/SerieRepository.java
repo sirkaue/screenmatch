fbccb2c7a34935eb6254d786ba1f016407e2f6f7
sirkaue <kauekdslopes@gmail.com>
2024-06-30 17:55:06 -0300

    Updating repository

--- src/main/java/com/sirkaue/screenmatch/repository/SerieRepository.java
+++ src/main/java/com/sirkaue/screenmatch/repository/SerieRepository.java
@@ -31,4 +31,7 @@ public interface SerieRepository extends JpaRepository<Serie, Long> {
 
     @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE s = :serie AND YEAR (e.dataLancamento) >= :anoLancamento")
     List<Episodio> episodiosPorSerieEAno(Serie serie, Integer anoLancamento);
+
+    @Query("SELECT s FROM Serie s JOIN s.episodios e GROUP BY s ORDER BY MAX (e.dataLancamento) DESC LIMIT 5")
+    List<Serie> latestReleases();
 }
