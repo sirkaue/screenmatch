640f6d746d995507fced2e602d7111500ab2fc75
sirkaue <kauekdslopes@gmail.com>
2024-06-30 13:47:26 -0300

    new ScreenmatchApplication

--- src/main/java/com/sirkaue/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/sirkaue/screenmatch/ScreenmatchApplication.java
@@ -1,25 +1,12 @@
 package com.sirkaue.screenmatch;
 
-import com.sirkaue.screenmatch.menu.Menu;
-import com.sirkaue.screenmatch.repository.SerieRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication implements CommandLineRunner {
-
-    @Autowired
-    private SerieRepository repository;
+public class ScreenmatchApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ScreenmatchApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        Menu menu = new Menu(repository);
-        menu.exibeMenu();
     }
 }
