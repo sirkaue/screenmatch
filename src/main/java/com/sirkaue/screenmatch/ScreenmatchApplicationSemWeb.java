a3a1070d5b0bd89fa71c04837b4cd125a85789bf
sirkaue <kauekdslopes@gmail.com>
2024-06-30 13:45:32 -0300

    refatorando codigo

---
+++ src/main/java/com/sirkaue/screenmatch/ScreenmatchApplicationSemWeb.java
@@ -0,0 +1,25 @@
+package com.sirkaue.screenmatch;
+
+import com.sirkaue.screenmatch.menu.Menu;
+import com.sirkaue.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+//public class ScreenmatchApplicationSemWeb implements CommandLineRunner {
+//
+//    @Autowired
+//    private SerieRepository repository;
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(ScreenmatchApplicationSemWeb.class, args);
+//    }
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        Menu menu = new Menu(repository);
+//        menu.exibeMenu();
+//    }
+//}
